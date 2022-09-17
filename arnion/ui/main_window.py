674dd1dd115a5e57cc5a26cb00870e2f38504903
andriyashchenko <108915770+andriyashchenko@users.noreply.github.com>
2022-09-17 20:40:20 +0300

    Работа с MySQL conection

--- arnion/ui/main_window.py
+++ arnion/ui/main_window.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 
+from arnion.db.mysql_connection import ConnectionHandler
+
 
 class MainWindow:
 
@@ -44,11 +46,22 @@ class MainWindow:
                               font=('Helvetika', 10, 'bold'), bg='#ccffcc')
         btn_close.place(x=160, y=200, width=120, height=50)
 
+        # Добавление кнопки "Тест"
+        btn_test = tk.Button(self.window, text='Тест',
+                              font=('Helvetika', 10, 'bold'),
+                              bg='#ffffcc', command=self.do_test)
+        btn_test.place(x=25, y=300, width=120, height=50)
+
         #Добавление кнопки закрытия программы
         btn_close = tk.Button(self.window, text='Выход',
                               font=('Helvetika', 10, 'bold'),
                               bg='#ccffcc', command=self.close)
         btn_close.place(x=160, y=300, width=120, height=50)
+
+    # Функция "Тест"
+    def do_test(self):
+        ch = ConnectionHandler()
+        ch.do_test()
 
     # Функция закрытия главного окна программы
     def close(self):
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import tkinter as tk
+from tkinter import messagebox as mb
+
+from arnion.db.mysql_connection import my_connection_handler
 from arnion.ui.main_window import MainWindow
 
 # Точка входа в программу
 def main():
+
+    chk = my_connection_handler.check_connection()
+    if not chk:
+        root = tk.Tk()
+        root.withdraw()
+        mb.showerror(parent=None, title="Ошибка", message="Отсутствие соединения с MySQL сервером.")
+        return
+
     main_window = MainWindow()
     main_window.start_mainloop()
+
     return 0
 
 if __name__ == '__main__':
