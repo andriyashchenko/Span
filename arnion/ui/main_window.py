6ddceff4ec55dcc62fdd27aacdd66578f9a78ed5
andriyashchenko <108915770+andriyashchenko@users.noreply.github.com>
2022-09-19 14:50:39 +0300

    Создание отчетов

--- arnion/ui/main_window.py
+++ arnion/ui/main_window.py
@@ -5,6 +5,8 @@ from arnion.data.departmens_data import DepartmentDataHandler
 from arnion.data.employees_data import EmployeeDataHandler
 
 from arnion.db.mysql_connection import ConnectionHandler
+from arnion.ui.departments_reports_ui import DepartmentsReportWindow
+from arnion.ui.employees_reports_ui import EmployeesReportWindow
 
 
 class MainWindow:
@@ -16,49 +18,49 @@ class MainWindow:
         self.window.title('SPAN')
 
         # Добавление метки заголовка
-        lbl_title = tk.Label(text='SPAN', font=('Helvetika', 16, 'bold'),
+        lbl_title = tk.Label(text='SPAN', font=('Helvetica', 16, 'bold'),
                               fg='#0000cc', justify='center')
         lbl_title.place(x=25, y=15, width=250, height=50)
 
         # Добавление метки заголовка данных
-        lblTitle1 = tk.Label(text='Данные', font=('Helvetika', 12, 'bold'),
+        lblTitle1 = tk.Label(text='Данные', font=('Helvetica', 12, 'bold'),
                              fg='#0066ff', justify='center')
         lblTitle1.place(x=25, y=55, width=250, height=50)
 
         # Добавление кнопки данных "Отделы"
         btn_report = tk.Button(self.window, text='Отделы',
-                              font=('Helvetika', 10, 'bold'), bg='#ccffcc')
+                              font=('Helvetica', 10, 'bold'), bg='#ccffcc')
         btn_report.place(x=25, y=100, width=120, height=50)
 
         # Добавление кнопки данных "Сотрудники"
         btn_close = tk.Button(self.window, text='Сотрудники',
-                              font=('Helvetika', 10, 'bold'), bg='#ccffcc')
+                              font=('Helvetica', 10, 'bold'), bg='#ccffcc')
         btn_close.place(x=160, y=100, width=120, height=50)
 
         # Добавление метки заголовка отчетов
-        lblTitle1 = tk.Label(text='Данные', font=('Helvetika', 12, 'bold'),
+        lblTitle1 = tk.Label(text='Данные', font=('Helvetica', 12, 'bold'),
                              fg='#0066ff', justify='center')
         lblTitle1.place(x=25, y=155, width=250, height=50)
 
         # Добавление кнопки отчетов "Отделы"
-        btn_report = tk.Button(self.window, text='Отделы',
-                               font=('Helvetika', 10, 'bold'), bg='#ccffcc')
-        btn_report.place(x=25, y=200, width=120, height=50)
+        btn_report_departments = tk.Button(self.window, text='Отделы',
+                               font=('Helvetica', 10, 'bold'), bg='#ccffcc', command=self.do_report_departments)
+        btn_report_departments.place(x=25, y=200, width=120, height=50)
 
         # Добавление кнопки отчетов "Сотрудники"
-        btn_close = tk.Button(self.window, text='Сотрудники',
-                              font=('Helvetika', 10, 'bold'), bg='#ccffcc')
-        btn_close.place(x=160, y=200, width=120, height=50)
+        btn_report_employees = tk.Button(self.window, text='Сотрудники',
+                              font=('Helvetica', 10, 'bold'), bg='#ccffcc', command=self.do_report_employees)
+        btn_report_employees.place(x=160, y=200, width=120, height=50)
 
         # Добавление кнопки "Тест"
         btn_test = tk.Button(self.window, text='Тест',
-                              font=('Helvetika', 10, 'bold'),
+                              font=('Helvetica', 10, 'bold'),
                               bg='#ffffcc', command=self.do_test)
         btn_test.place(x=25, y=300, width=120, height=50)
 
         #Добавление кнопки закрытия программы
         btn_close = tk.Button(self.window, text='Выход',
-                              font=('Helvetika', 10, 'bold'),
+                              font=('Helvetica', 10, 'bold'),
                               bg='#ccffcc', command=self.close)
         btn_close.place(x=160, y=300, width=120, height=50)
 
@@ -67,6 +69,16 @@ class MainWindow:
         employees = EmployeeDataHandler.select_list()
         for employee in employees:
             print(employee.get_full_name())
+
+    # Открытие отчета "Отделы"
+    def do_report_departments(self):
+        rpt = DepartmentsReportWindow()
+        rpt.open()
+
+    # Открытие отчета "Сотрудники"
+    def do_report_employees(self):
+        rpt = EmployeesReportWindow()
+        rpt.open()
 
     # Функция закрытия главного окна программы
     def close(self):
